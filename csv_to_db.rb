7559c914fc988ef8300e6fc0002ab8f930f72ed9
Martin Becker <mbeckerwork@gmail.com>
2016-09-20 23:07:37 +0000

    changes data input file

--- csv_to_db.rb
+++ csv_to_db.rb
@@ -1,20 +1,21 @@
 require 'csv'
 require 'mongoid'
 
-  root_path = File.dirname(__FILE__)
+root_path = File.dirname(__FILE__)
 
-  Mongoid.load!("#{root_path}/mongoid.yml", :development )
-  
-  CSV.foreach("lotto_res.csv",{headers: false}) do |lotto_res|
-    d,d2 = lotto_res[0].split('|')
-    Lotto_Result.create(
-      {
-        draw_date: DateTime.new(*d[0].split(',').map(&:to_i)),
-        printable_date: d2,
-        numbers: lotto_res[1..6].map {|e| e.to_i},
-        bonus: lotto_res[7].to_i,
-        jackpot: lotto_res[8]
-      }
-    )
+Mongoid.load!("#{root_path}/mongoid.yml", :development )
 
-  end
+CSV.foreach("lotto_res.csv",{headers: false}) do |lotto_res|
+  d,d2 = lotto_res[0].split('|')
+  lr = Lotto_Result.new(
+    {
+      draw_date: DateTime.new(*d[0].split(',').map(&:to_i)),
+      printable_date: d2,
+      numbers: lotto_res[1..6].map {|e| e.to_i},
+      bonus: lotto_res[7].to_i,
+      jackpot: lotto_res[8]
+    }
+  )
+  lr.save
+
+end
